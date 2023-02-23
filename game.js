2c3cc3e9c89b59d51ac79d6735c31af968fa8987
mhmd-bal <mohammad.balouza@gmail.com>
2023-02-23 15:34:30 +0200

    Added Basic game function structure, Added Basic game Start and lose

--- game.js
+++ game.js
@@ -4,13 +4,23 @@ window.onload = function(){
     var Status = document.getElementById("status");
     var Boundaries = document.getElementsByClassName("boundary");
     var Game_started = false;
+    var Lost_beforestart = false;
+    var Score = 0;
 
+    // Game Functions:
 
     function gameState() {
-        if(Game_started){
-            console.log("Start has already been clicked");
+        if(Game_started && Lost_beforestart){
+            for (var i=0; i < Boundaries.length; i++) {
+                Boundaries[i].classList.toggle("youlose");
+            }
+            Lost_beforestart = false;
         }
-        else{
+        if(!Game_started && Lost_beforestart){
+            for (var i=0; i < Boundaries.length; i++) {
+                Boundaries[i].classList.toggle("youlose");
+            }
+            Lost_beforestart = false;
             console.log("Start is clicked!");
         }
         Game_started = true;
@@ -18,7 +28,39 @@ window.onload = function(){
 
     function gameStart() {
         Start.addEventListener('click', gameState);
+        gamePlay();
     }
+
+    function gamePlay() {
+        for (var i=0; i < Boundaries.length; i++) {
+            Boundaries[i].addEventListener('mouseover', gameLost);
+        }
+    }
+
+    function gameLost() {
+        if(Game_started){
+            for (var i=0; i < Boundaries.length; i++) {
+                Boundaries[i].classList.toggle("youlose");
+            }
+            Status.textContent = "You Lose!";
+            Game_started = false;
+            Lost_beforestart = true;
+        }
+    }
+
+    function gameWon() {
+        
+    }
+
+    function gameEnd() {
+
+    }
+
+    function scoreReset() {
+
+    }
+
+    // The actual game
 
     gameStart();
  }
