f5f83af02779512b07b340689354e480403ccc61
mhmd-bal <mohammad.balouza@gmail.com>
2023-02-23 14:55:36 +0200

    Added the functionality of the js file, Added Game start function and indication

--- game.js
+++ game.js
@@ -1,6 +1,25 @@
-let Start = document.getElementById("start");
-let End = document.getElementById("end");
-let Status = document.getElementById("status");
-let Boundaries = document.getElementsByClassName("boundary");
+window.onload = function(){
+    var Start = document.getElementById("start");
+    var End = document.getElementById("end");
+    var Status = document.getElementById("status");
+    var Boundaries = document.getElementsByClassName("boundary");
+    var Game_started = false;
 
+
+    function gameState() {
+        if(Game_started){
+            console.log("Start has already been clicked");
+        }
+        else{
+            console.log("Start is clicked!");
+        }
+        Game_started = true;
+    }
+
+    function gameStart() {
+        Start.addEventListener('click', gameState);
+    }
+
+    gameStart();
+ }
 
