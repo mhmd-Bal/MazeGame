617388b4ebc4d7f50fef15d56950ea405476758f
mhmd-bal <mohammad.balouza@gmail.com>
2023-02-23 14:02:39 +0200

    Added the necessary variables

---
+++ game.js
@@ -0,0 +1,6 @@
+let Start = document.getElementById("start");
+let End = document.getElementById("end");
+let Status = document.getElementById("status");
+let Boundaries = document.getElementsByClassName("boundary");
+
+
